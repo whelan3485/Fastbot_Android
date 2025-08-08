565d27a52e6db4d70c81779aa84d62582aca5a92
whelan3485 <whelan3485@gmail.com>
2025-08-08 21:27:53 +0800

    for sdk36

--- monkey/src/main/java/com/android/commands/monkey/events/base/MonkeyRotationEvent.java
+++ monkey/src/main/java/com/android/commands/monkey/events/base/MonkeyRotationEvent.java
@@ -1,47 +1,38 @@
-/*
- * Copyright (c) 2020 Bytedance Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android.commands.monkey.events.base;
 
 import android.app.IActivityManager;
-import android.os.RemoteException;
+// IWindowManager 不再需要直接作为参数，但为了兼容旧的 injectEvent 签名，可以保留
 import android.view.IWindowManager;
-
 import com.android.commands.monkey.events.MonkeyEvent;
 import com.android.commands.monkey.utils.Logger;
+import java.io.IOException;
 
-/**
- * monkey screen rotation event
- */
 public class MonkeyRotationEvent extends MonkeyEvent {
-
     public final int mRotationDegree;
     public final boolean mPersist;
 
-    /**
-     * Construct a rotation Event.
-     *
-     * @param degree  Possible rotation degrees, see constants in
-     *                anroid.view.Suface.
-     * @param persist Should we keep the rotation lock after the orientation change.
-     */
     public MonkeyRotationEvent(int degree, boolean persist) {
         super(EVENT_TYPE_ROTATION);
         mRotationDegree = degree;
         mPersist = persist;
+    }
+
+    private int executeShellCommand(String command) {
+        try {
+            // 在 shell 环境中执行命令
+            Process process = Runtime.getRuntime().exec(command);
+            // 等待命令执行完成，检查是否有错误
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                Logger.warningPrintln("Shell command failed with exit code " + exitCode + " for: " + command);
+                return MonkeyEvent.INJECT_FAIL;
+            }
+            return MonkeyEvent.INJECT_SUCCESS;
+        } catch (IOException | InterruptedException e) {
+            Logger.errorPrintln("Failed to execute shell command: " + command);
+            e.printStackTrace();
+            return MonkeyEvent.INJECT_FAIL;
+        }
     }
 
     @Override
@@ -50,15 +41,26 @@ public class MonkeyRotationEvent extends MonkeyEvent {
             Logger.println(":Sending rotation degree=" + mRotationDegree + ", persist=" + mPersist);
         }
 
-        // inject rotation event
-        try {
-            iwm.freezeRotation(mRotationDegree);
-            if (!mPersist) {
-                iwm.thawRotation();
-            }
-            return MonkeyEvent.INJECT_SUCCESS;
-        } catch (RemoteException ex) {
-            return MonkeyEvent.INJECT_ERROR_REMOTE_EXCEPTION;
+        // 步骤 1: 关闭自动旋转以准备锁定
+        if (executeShellCommand("settings put system accelerometer_rotation 0") != MonkeyEvent.INJECT_SUCCESS) {
+            return MonkeyEvent.INJECT_FAIL;
         }
+
+        // 步骤 2: 设置固定的旋转方向
+        if (executeShellCommand("settings put system user_rotation " + mRotationDegree) != MonkeyEvent.INJECT_SUCCESS) {
+            // 如果失败，最好尝试恢复自动旋转
+            executeShellCommand("settings put system accelerometer_rotation 1");
+            return MonkeyEvent.INJECT_FAIL;
+        }
+
+        // 步骤 3: 如果不是持久化锁定，则恢复自动旋转 (模拟 thaw)
+        if (!mPersist) {
+            if (verbose > 0) {
+                Logger.println(":Thawing rotation lock.");
+            }
+            return executeShellCommand("settings put system accelerometer_rotation 1");
+        }
+
+        return MonkeyEvent.INJECT_SUCCESS;
     }
 }
